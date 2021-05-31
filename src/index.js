6599cbc367cfd3da21203b8ae255532df1969e38
ishimwebernard <isbernard2001@gmail.com>
2021-06-01 00:32:09 +0200

    mend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import Express from "express"
 import Routes from './routes/index'
 import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+dotenv.config();
+
 const app = Express();
 
 app.use(bodyParser.json());
 app.use("/",Routes);
-app.listen(5000, ()=>{ 
+app.listen(process.env.PORT || 5000, ()=>{ 
     console.log("App started");
 });
 export default app;
