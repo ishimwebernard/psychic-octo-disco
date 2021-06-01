e27c545b634d9e8b44d6d02069927defb5a8bfdd
ishimwebernard <isbernard2001@gmail.com>
2021-06-01 16:21:40 +0200

    mend

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import Express from "express"
 import Routes from './routes/index'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config();
 
 const app = Express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/",Routes);
 app.listen(process.env.PORT || 5000, ()=>{ 
     console.log("App started");
